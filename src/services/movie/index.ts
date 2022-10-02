f7a15049b81c7e0b9b683ee0894b412c305f2aa8
Camila Nery <camila.nery@sanar.com>
2022-10-02 13:47:01 -0300

    feat: error treatment for services

--- src/services/movie/index.ts
+++ src/services/movie/index.ts
@@ -3,21 +3,26 @@ import { IMovie } from "@/domain/Movie";
 import { getMovieRecommendationRail } from "@/services/rails";
 
 const getMovie = async (id: string): Promise<IMovie> => {
-  const movie = await tmdbClient.request<ITMDBMovieDetails>(`/movie/${id}`);
+  try {
+    const movie = await tmdbClient.request<ITMDBMovieDetails>(`/movie/${id}`);
 
-  const recommendations = await getMovieRecommendationRail(id);
-  return {
-    posterUrl: tmdbClient.buildImageUrl(movie.poster_path),
-    backdropUrl: tmdbClient.buildImageUrl(movie.backdrop_path),
-    title: movie.title,
-    ...(movie.original_title && { originalTitle: movie.original_title }),
-    ...(movie.release_date && { releaseDate: movie.release_date }),
-    ...(movie.tagline && { tagline: movie.tagline }),
-    ...(movie.runtime && { runtime: movie.runtime }),
-    ...(movie.overview && { description: movie.overview }),
-    genres: movie.genres?.map((genre) => genre.name) || [],
-    recommendations,
-  };
+    const recommendations = await getMovieRecommendationRail(id);
+    return {
+      posterUrl: tmdbClient.buildImageUrl(movie.poster_path),
+      backdropUrl: tmdbClient.buildImageUrl(movie.backdrop_path),
+      title: movie.title,
+      ...(movie.original_title && { originalTitle: movie.original_title }),
+      ...(movie.release_date && { releaseDate: movie.release_date }),
+      ...(movie.tagline && { tagline: movie.tagline }),
+      ...(movie.runtime && { runtime: movie.runtime }),
+      ...(movie.overview && { description: movie.overview }),
+      genres: movie.genres?.map((genre) => genre.name) || [],
+      recommendations,
+    };
+  } catch (e) {
+    console.log("[ERROR] Movie details", e);
+    throw new Error("Error fetching movie details");
+  }
 };
 
 export { getMovie };
--- src/services/rails/index.ts
+++ src/services/rails/index.ts
@@ -12,33 +12,43 @@ const getHomeRails = async (): Promise<IRail[]> => {
   const rails = [
     { name: "Trending", path: "/trending/movie/week" },
     { name: "Top Rated", path: "/movie/top_rated" },
-    { name: "Action", path: "/discover/movie", filter: "with_genres=28" },
-    { name: "Comedy", path: "/discover/movie", filter: "with_genres=35" },
-    { name: "Drama", path: "/discover/movie", filter: "with_genres=18" },
+    { name: "Action", path: "/discover/movie", params: { with_genres: "28" } },
+    { name: "Comedy", path: "/discover/movie", params: { with_genres: "35" } },
+    { name: "Drama", path: "/discover/movie", params: { with_genres: "18" } },
   ];
 
   const homeRails: IRail[] = [];
 
   for (const rail of rails) {
     try {
-      const contentList = await tmdbClient.request<ITMDBList>(rail.path, rail.filter);
+      const contentList = await tmdbClient.request<ITMDBList>(rail.path, rail.params);
 
       if (contentList.results?.length) {
         homeRails.push({
           name: rail.name,
-          items: contentList.results.map(itemMapper),
+          items: contentList.results.map(buildRailItem),
         });
       }
-    } catch {}
+    } catch (e) {
+      console.log("[ERROR] Home rails", e);
+    }
+  }
+
+  if (!homeRails.length) {
+    throw new Error("Error fetching home rails");
   }
 
   return homeRails;
 };
 
 const getMovieRecommendationRail = async (movieId: string): Promise<IRailItem[]> => {
-  const rail = await tmdbClient.request<ITMDBList>(`/movie/${movieId}/recommendations`);
-
-  return rail.results?.map(itemMapper) || [];
+  try {
+    const rail = await tmdbClient.request<ITMDBList>(`/movie/${movieId}/recommendations`);
+    return rail.results?.map(buildRailItem) || [];
+  } catch (e) {
+    console.log("[ERROR] Movie recommendations", e);
+    return [];
+  }
 };
 
 export { getHomeRails, getMovieRecommendationRail };
